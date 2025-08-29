08108da91123e8e49a5b6a790411671dc3d2bbf0
Fatih Demir <fd1134@gmail.com>
2025-08-29 11:53:36 +0300

    add modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import 'modern-normalize'
+import "./index.css";
 
-import './index.css'
+import App from "./components/App/App";
 
-import App from './components/App/App'
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,46 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import storage from "redux-persist/lib/storage";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const favouritesPersistConfig = {
+  key: "favourites",
+  storage,
+};
+
+const filtersPersistConfig = {
+  key: "filters",
+  storage,
+};
+
+
+import { filtersReducer } from './filters/slice';
+import { favouritesReducer } from './favourites/slice';
 import vehiclesReducer from "./vehicles/vehiclesSlice";
-import reviewsReducer from "./reviews/reviewsSlice";
-import filtersReducer from "./filters/filtersSlice";
-import favouritesReducer from "./favourites/favouritesSlice";
+
+
 
 export const store = configureStore({
   reducer: {
     vehicles: vehiclesReducer,
-    reviews: reviewsReducer,
-    filters: filtersReducer,
-     favourites: favouritesReducer
+    filters: persistReducer(filtersPersistConfig, filtersReducer),
+    favourites: persistReducer(favouritesPersistConfig, favouritesReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
